f7d96cd410675e69be3246fc5963ac9a0f6ecaf2
Ran Zhuo <ryanne@Rans-MacBook-Air.local>
2022-06-29 22:31:11 -0400

    cool stuff

--- tinterest/main_app/urls.py
+++ tinterest/main_app/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
   path('', views.home, name='home'),
-  path('welcome', views.welcome, name='welcome'),
+  path('welcome/', views.welcome, name='welcome'),
   path('accounts/signup/', views.signup, name='signup'),
   
   #temperate profile url
