39048eec27f3c703430ab53d175a6aa194229535
Ran Zhuo <ryanne@Rans-MacBook-Air.local>
2022-07-09 08:45:22 -0400

    redirect

--- tinterest/main_app/views.py
+++ tinterest/main_app/views.py
@@ -47,7 +47,7 @@ def signup(request):
     if form.is_valid():
       user = form.save()
       login(request,user)
-      return redirect('home')
+      return redirect('profile')
     else: 
       error_message = 'Invalid sign up- try again'
   form = UserCreationForm()
